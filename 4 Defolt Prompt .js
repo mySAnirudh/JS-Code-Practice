2a121ed20427c46ba4631e5306372cf89ce61a33
Anirudh Kumar <96437957+mySAnirudh@users.noreply.github.com>
2021-12-27 23:36:04 +0530

    Add files via upload

---
+++ 4 Defolt Prompt .js
@@ -0,0 +1,11 @@
+let age = prompt ('Enter Your Age',20); // 20 Is a defolt age
+
+if(age != null){
+    document.write('Your Age is ${age}');
+
+}
+
+else {
+    document.write('Age is not Field');
+
+}
