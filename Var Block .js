8466371f8d4d1ec2ce5eccfc351650265d624a18
Anirudh Kumar <96437957+mySAnirudh@users.noreply.github.com>
2022-05-24 23:55:55 +0530

    Add files via upload

---
+++ Var Block .js
@@ -0,0 +1,6 @@
+//hoisted to top ; var i = undefined
+for (var i=1;i<=5;i++){
+    console.log(i);
+   }
+   console.log("after the loop i value is "+i);
+   
---
+++ var and block level safety .js
@@ -0,0 +1,4 @@
+var balance = 5000
+console.log(typeof balance)
+var balance = {message:"hello"}
+console.log(typeof balance)
