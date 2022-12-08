a0049f9b07d7e9fa2638c46d4d0bb2208d1562d1
Anirudh Kumar <96437957+mySAnirudh@users.noreply.github.com>
2022-12-08 21:51:54 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,2 @@
-const http =require('http');
-const { json } = require('stream/consumers');
-http.createServer((req,resp)=>{
-    resp.writeHead(200,{'Content-Type':'application\json'});
-    resp.write(JSON.stringify({name:'Samrat Singh', email:'mynsamrat@gmail.com'}));
-    resp.end();
-}).listen(5002); // It's start after run in cmd "nodemon start " this cammand 
-// Then run in broser then it's show the output 
-
-
-// RUN in Thunder Client by this code in Get method :- http://localhost:5000
-// Don't use https 
+const h1=require('h1').writeFileSync;
+h1("hellotc.txt","avf")
